68c92549508f1d60dbd5353a5eef4c2590678319
username <email@email.com>
2016-02-29 11:15:22 +0200

    some change after stash

--- MarketingWebApp/MarketingWebApp/Global.asax.cs
+++ MarketingWebApp/MarketingWebApp/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,3 +22,5 @@ namespace MarketingWebApp
     }
   }
 }
+
+//made after stash
